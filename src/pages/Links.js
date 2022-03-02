28a1ed352b1dbdd3064ecd0c31b25e586d14278d
JoeJorgensen <joebjorg@gmail.com>
2022-03-01 18:21:17 -0700

    crud funtions

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -1,10 +1,19 @@
-import React from "react";
-
+import React, {useContext} from "react";
+import {DataContext} from "../providers/DataProvider";
 const Links = ()=>{
-
+const {links, getLinks, createLink, updateLink, deleteLink} = useContext(DataContext)
     return  (
 <div>
+
     <h1> Links Page </h1>
+    {JSON.stringify(links)}
+    <button onClick={getLinks}>Get Links</button>
+    <button onClick={()=>createLink({ title:'New Title', username:'joej'})}>Create Link</button>
+    <button onClick={()=>updateLink({id:200, title:'Update', username:'joej'})}>Update Link</button>
+    <button onClick={()=>deleteLink(200)}>Delete Link</button>
+
+
+
 </div>
 
     )
--- src/providers/DataProvider.js
+++ src/providers/DataProvider.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 
 // createContext HERE this doing a lot for
@@ -5,12 +6,62 @@ import React, { useState } from "react";
 export const DataContext = React.createContext();
 
 const DataProvider = (props) => {
-  const [dataDemo, setDataDemo] = useState('dataDemo from provider');
+  const [links, setLinks] = useState([]);
 
+  const baseUrl = 'https://link-app-sp22.herokuapp.com'
+
+  const getLinks = async()=>{
+try{
+  let res = await axios.get(`${baseUrl}/api/links`)
+  console.log(res)
+  setLinks(res.data)
+}catch(err){
+  alert('error occurred')
+}
+  }
+
+  const createLink = async(linkObj)=>{
+    try{
+      let res = await axios.post(`${baseUrl}/api/links/${linkObj.id}`, linkObj)
+
+      setLinks([res.data,...links])
+    }catch(err){
+      alert('error occurred')
+    }
+  }
+
+  const updateLink = async(linkObj)=>{
+    if(!linkObj.id) {
+    alert('No ID given')
+    return
+    }
+    try{
+      let res = await axios.put(`${baseUrl}/api/links`, linkObj)
+
+      let updateLinks = links.map(l=> l.id === res.data.id ? res.data : l )
+      setLinks(updateLinks)
+    }catch(err){
+      alert('error occurred')
+    }
+  }
+
+  const deleteLink = async (id)=>{
+    
+    try{
+      let res = await axios.delete(`${baseUrl}/api/links/${id}`)
+      setLinks(links.filter(l=> l.id !== id))
+    }catch{
+      alert('error occurred')
+    }
+  }
   // create an object that will be 'global state'
   const dataProviderThing = {
-    dataDemo,
-    setDataDemo
+  createLink,
+  updateLink,
+  deleteLink,
+   getLinks,
+   links, 
+    
 };
   // return the provider which will wrap my all app
   return (
